2cfe7c5d5e7425c645d1dce31dc91a1dc89d62ef
yoea13 <yoea13@gmail.com>
2023-03-15 12:19:41 -0300

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,21 @@ import {
   Input,
   SimpleGrid,
   Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useState } from 'react';
+} from "@chakra-ui/react";
+import { Alchemy, Network } from "alchemy-sdk";
+import { useState } from "react";
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
+  const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
 
   async function getNFTsForOwner() {
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
-      network: Network.ETH_MAINNET,
+      apiKey: "fsAGVn-r_oZ8J7Ugys_23bO5GR-e76M4",
+      network: Network.MATIC_MAINNET,
+      // network: Network.ETH_MAINNET,
     };
 
     const alchemy = new Alchemy(config);
@@ -45,9 +46,9 @@ function App() {
     <Box w="100vw">
       <Center>
         <Flex
-          alignItems={'center'}
+          alignItems={"center"}
           justifyContent="center"
-          flexDirection={'column'}
+          flexDirection={"column"}
         >
           <Heading mb={0} fontSize={36}>
             NFT Indexer 🖼
@@ -61,7 +62,7 @@ function App() {
         w="100%"
         flexDirection="column"
         alignItems="center"
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
         <Input
@@ -80,14 +81,14 @@ function App() {
         <Heading my={36}>Here are your NFTs:</Heading>
 
         {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
             {results.ownedNfts.map((e, i) => {
               return (
                 <Flex
-                  flexDir={'column'}
+                  flexDir={"column"}
                   color="white"
                   bg="blue"
-                  w={'20vw'}
+                  w={"20vw"}
                   key={e.id}
                 >
                   <Box>
@@ -99,7 +100,7 @@ function App() {
             })}
           </SimpleGrid>
         ) : (
-          'Please make a query! The query may take a few seconds...'
+          "Please make a query! The query may take a few seconds..."
         )}
       </Flex>
     </Box>
